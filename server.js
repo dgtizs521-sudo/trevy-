352a471c38ca7b3d3a02d4db7fd9b9b2ab756f29
dgtizs521-sudo <dgtizs521@gmail.com>
2026-07-01 02:27:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const taskRoutes = require("./routes/tasks");
 const withdrawalRoutes = require("./routes/withdrawals");
 const transactionRoutes = require("./routes/transactions");
 const summaryRoutes = require("./routes/summary");
+const adminRoutes = require("./routes/admin");
 
 ensureFile();
 
@@ -18,7 +19,6 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// Tiny request logger
 app.use((req, res, next) => {
   const start = Date.now();
   res.on("finish", () => {
@@ -34,13 +34,12 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/withdrawals", withdrawalRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/summary", summaryRoutes);
+app.use("/api/admin", adminRoutes);
 
-// 404 handler
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found." });
 });
 
-// Centralized error handler
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: "Something went wrong on the server." });
